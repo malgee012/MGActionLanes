f828e78aed3b3577382899fc4f2378703a6dfa06
maling <951684507@qq.com>
2021-01-01 20:38:00 +0800

    更新-删除cocoapods的索引库

--- fastlane/ios_fastfile.rb
+++ fastlane/ios_fastfile.rb
@@ -18,7 +18,7 @@ platform :ios do
     is_public = "true".eql?(public_str)
 
 
-    UI.message(">>>>>  #{is_public}   #{"false".eql?(is_public)}   #{"false" == is_public}")
+    UI.message(">>>>>  #{public_str}   #{is_public}   #{"false".eql?(is_public)}   #{"false" == is_public}")
 
     UI.message("👉 代码库名字: #{libName}, tag版本: #{tag}, 提交信息: #{message}, public:#{is_public} ")
 
